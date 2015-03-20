4c1bf443d95089706032edec177b06634a953c86
mykelangellopez <mlopez.99@kippnyccp.org>
2015-03-20 13:21:52 -0400

    Update application.js

--- shoppinglist/application.js
+++ shoppinglist/application.js
@@ -7,3 +7,9 @@
     <script type="text/javascript" src="http://code.jquery.com/jquery-2.0.0.min.js"></script>
     <script type="text/javascript" src="application.js"></script>
   </head>
+$(document).ready(function(){
+  $('#submit').on('click', function(){
+    var entry = $('input').val();
+    $('ul').append("<li>"+entry+"</li>");
+  })
+});
